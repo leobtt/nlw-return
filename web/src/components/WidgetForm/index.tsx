7d17c45b4c241e2d93e2a9907f577d0dbad4689f
leobtt <leonarbat8@gmail.com>
2022-05-04 15:34:57 -0300

    feedback interface

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+import bugImage from '../../assets/bug.svg'
+import lampImage from '../../assets/lamp.svg'
+import thoughtImage from '../../assets/thought.svg'
+
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImage,
+      alt: 'Imagem de um inseto'
+    }
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: lampImage,
+      alt: 'Imagem de uma lâmpada'
+    }
+  },
+  THOUGHT: {
+    title: 'Outro',
+    image: {
+      source: thoughtImage,
+      alt: 'Imagem de um balão de pensamento'
+    }
+  }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
+
+  function handleRestartFeedback() {
+    setFeedbackType(null)
+    setFeedbackSent(false)
+  }
+
+  return (
+    <div
+      className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-3rem)] md:w-auto"
+    >
+
+
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestarted={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestarted={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className='text-xs text-neutral-400'>
+        Feito com 💜 pela &nbsp;
+        <a
+          href="https://rocketseat.com.br/"
+          className="underline underline-offset-2"
+        >
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  )
+}
