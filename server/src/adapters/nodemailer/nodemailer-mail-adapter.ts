1671d8457f220419c9f5c2c72f68c8016d7946fd
leobtt <leonarbat8@gmail.com>
2022-05-07 21:12:57 -0300

    adding 'mailgun'

--- server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -1,22 +1,27 @@
 import { MailAdapter, SendMailData } from "../mail-adapter";
 import nodemailer from 'nodemailer'
+import nodeMailgun, { Options } from 'nodemailer-mailgun-transport'
+import 'dotenv/config'
 
-const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+
+const auth = {
   auth: {
-    user: "bb2fb17565715e",
-    pass: "a2c84c3520aa37"
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
   }
-});
+}
+
+const transport = nodemailer.createTransport(nodeMailgun(auth as Options))
 
 export class NodemailerEmailAdpter implements MailAdapter {
   async sendEmail({ subject, body }: SendMailData) {
     await transport.sendMail({
-      from: 'Equipe Feedget <oi@feedget.com>',
-      to: 'Leonardo Batista <leonarbat8@gmail.com>',
+      from: 'Equipe Feedget <me@samples.mailgun.org>',
+      to: 'leonarbat8@gmail.com',
       subject,
       html: body
+    }, (err, data) => {
+      if (err) console.log(err)
     })
   }
 }
